86380be7affaff05a86559f3430de58baad6b176
N1kOk <n1k_ok@bk.ru>
2023-03-28 12:48:03 +0500

    fix: remove extra files

--- dist/dev.d.ts
+++
@@ -1,8 +0,0 @@
-import './shared/env';
-import { TypedData } from 'ydb-sdk';
-export declare class Test extends TypedData {
-    id: number;
-    name: string;
-    phone: number;
-    ttl: string;
-}
--- src/shared/env.ts
+++
@@ -1,6 +0,0 @@
-import * as dotenv from 'dotenv'
-import path from 'path'
-
-process.env.YDB_SERVICE_ACCOUNT_KEY_FILE_CREDENTIALS = path.resolve(__dirname, '../../config/yc-account.json')
-
-dotenv.config()
